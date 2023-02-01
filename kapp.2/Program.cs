1f0766670231153e0d41b4e91244a8599ee9e692
Admin <Admin@TEB127001>
2023-02-01 10:50:12 +0100

    Add project files.

---
+++ kapp.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace uppgift4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lönen för 3 anställda är ");
+            double lön1 = double.Parse(Console.ReadLine());
+            double lön2 = double.Parse(Console.ReadLine());
+            double lön3 = double.Parse(Console.ReadLine());
+            double lön = (lön1 + lön2 + lön3) / 3;
+            Console.WriteLine("Medellönen för personalen är " + lön); ///jhjhj
+
+        }
+    }
+}
